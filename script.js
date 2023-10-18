fc9c541717e4a8570990d5c043c8dabc0d714ebc
Lucas da silva santiago <84160457+caslu26@users.noreply.github.com>
2023-10-18 18:58:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+ 
+const numeroWhatsApp = '11948391523'; // Substitua pelo seu número de WhatsApp
+const mensagem = 'Olá, estou interessado em suas habilidades!'; // Defina a mensagem desejada
+const urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
+// Função para calcular e exibir as porcentagens de habilidades
+function calcularPorcentagens() {
+    const skills = {
+        "HTML": 90,
+        "CSS": 80,
+        "JavaScript": 75,
+        "React": 70,
+        "Node.js": 65,
+        "SQL": 60,
+        "Python": 90,
+        "Django": 40,
+        "Flask": 30,
+        "API Rest": 50,
+        "Pandas": 70,
+        "Selenium": 80
+    };
+
+    const updatePercentage = (skill) => {
+        skills[skill] = Math.min(skills[skill] + Math.random() * 5, 100);
+        const progressBar = document.getElementById(`${skill.toLowerCase()}-progress`);
+        progressBar.value = skills[skill];
+
+        // Atualize a pseudo-classe progress::after com o novo valor
+        const pseudoElement = window.getComputedStyle(progressBar, 'progress::after');
+        pseudoElement.content = `'${Math.round(skills[skill])}%'`;
+
+        const label = progressBar.previousElementSibling;
+        label.textContent = `${skill} ${Math.round(skills[skill])}%`;
+    };
+
+    for (const skill in skills) {
+        updatePercentage(skill);
+    }
+
+    // Atualize as porcentagens periodicamente
+    setInterval(() => {
+        for (const skill in skills) {
+            updatePercentage(skill);
+        }
+    }, 1000); // Atualize a cada segundo (1000 milissegundos)
+}
+
+// Chama a função quando a página estiver carregada
+window.addEventListener("load", calcularPorcentagens);
+
+
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        const slides = document.querySelectorAll(".mySlides");
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slideIndex++;
+
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 10000); // Mude a imagem a cada 10 segundos (10000ms)
+    }
+
